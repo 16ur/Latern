69981a8b0de52c5ab619bf523ec7009cc8eb9cf7
16ur <akuseruuu@protonmail.com>
2026-05-13 22:57:44 +0200

    chore(dev): proxy frontend api requests to backend

--- latern-backend/latern_back/settings.py
+++ latern-backend/latern_back/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-t!p77(#(d%(v(s(shuwlzs&e7ib5k#0lvg5twl1i0i8--m#s#&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "backend"]
 
 
 # Application definition
